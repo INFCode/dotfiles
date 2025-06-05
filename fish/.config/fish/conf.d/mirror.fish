fdd3834526083e492ed2a8a9d29e8d8c3975a76d
INFCode <INFCode@163.com>
2025-06-05 11:36:44 +0800

    feat(fish): add huggingface mirror

--- fish/.config/fish/conf.d/mirror.fish
+++ fish/.config/fish/conf.d/mirror.fish
@@ -4,3 +4,6 @@ set -gx PIP_INDEX_URL https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 # RUSTUP
 set -gx RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 set -gx RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
+
+# HuggingFace
+set -gx HF_ENDPOINT https://hf-mirror.com/
