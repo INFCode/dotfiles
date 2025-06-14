99c61f460e264546a4a4836c470e50f5e2461604
INFCode <INFCode@163.com>
2025-06-14 21:55:45 +0800

    fix(fish,proxy): fix wrong proxy port

--- fish/.config/fish/functions/proxy_http.fish
+++ fish/.config/fish/functions/proxy_http.fish
@@ -1,6 +1,6 @@
 function proxy_http 
-    set -lx http_proxy  http://127.0.0.1:10809
-    set -lx https_proxy http://127.0.0.1:10809
+    set -lx http_proxy  http://127.0.0.1:10808
+    set -lx https_proxy http://127.0.0.1:10808
     set -lx HTTP_PROXY  $http_proxy
     set -lx HTTPS_PROXY $https_proxy
     eval $argv
