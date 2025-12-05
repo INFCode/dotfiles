10954f404c6d5d3a884e4380e09f6670c39e69d8
qifeiwu <qifeiwu@tencent.com>
2025-12-05 11:56:57 +0800

    update neovim

--- nvim/.config/nvim/lua/plugins/override.lua
+++ nvim/.config/nvim/lua/plugins/override.lua
@@ -8,11 +8,4 @@ return {
       colorscheme = "catppuccin",
     },
   },
-  {
-    -- This is a temporary fix as venv-selector is moving back to its main branch again
-    -- Remove this once the main branch is used for venv-selector
-    "linux-cultist/venv-selector.nvim",
-    branch = "main",
-    enabled = false,
-  },
 }
