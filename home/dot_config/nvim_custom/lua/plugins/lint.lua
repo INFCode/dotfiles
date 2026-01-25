ce25273cb8ca5e04188783fcb4b369241c8715eb
INFCode <INFCode@163.com>
2026-01-25 20:11:47 +0800

    feat(nvim): setup nvim-lint

--- home/dot_config/nvim_custom/lua/plugins/lint.lua
+++ home/dot_config/nvim_custom/lua/plugins/lint.lua
@@ -1,6 +1,11 @@
 MiniDeps.add("mfussenegger/nvim-lint")
+
+local all_linters_by_ft = {}
+
+local get_active_linters = function()
+  return _G.Custom.helpers.index(all_linters_by_ft, _G.Custom.config.languages)
+end
+
 MiniDeps.later(function()
-  require('lint').linters_by_ft = {
-    -- TODO: Fill this
-  }
+  require('lint').linters_by_ft = get_active_linters()
 end)
