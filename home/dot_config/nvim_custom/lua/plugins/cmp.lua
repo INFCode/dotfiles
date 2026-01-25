db2b2af1610b61d5755a929c966465398c08463e
INFCode <INFCode@163.com>
2026-01-25 18:42:44 +0800

    feat(nvim): configure blink

--- home/dot_config/nvim_custom/lua/plugins/cmp.lua
+++ home/dot_config/nvim_custom/lua/plugins/cmp.lua
@@ -2,3 +2,26 @@ MiniDeps.add({
   source = "saghen/blink.cmp",
   checkout = "v1.8.0",
 })
+
+
+MiniDeps.later(function()
+  require("blink.cmp").setup({
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = false
+        }
+      },
+      ghost_text = {
+        enabled = true,
+        show_without_selection = true
+      }
+    },
+    keymap = {
+      preset = 'enter',
+      ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+    }
+  })
+end)
